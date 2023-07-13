51ae65bac359bf073abd03ec7777fa639ab077e7
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2023-07-13 14:54:52 +0200

    add view class

--- App/Views/View.php
+++ App/Views/View.php
@@ -4,6 +4,10 @@ namespace App\Views;
 
 class View
 {
+    // ----------------------
+    // Instances
+    // ----------------------
+
     private array $data = [];
     private string $file;
 
@@ -13,6 +17,40 @@ class View
         $this->file = $file;
     }
 
+    // ----------------------
+    // Getters and Setters
+    // ----------------------
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+
+    public function getFile(): string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): void
+    {
+        $this->file = $file;
+    }
+
+    // ----------------------
+    // Methods
+    // ----------------------
+
+    public function addData(string $key, string $content): void
+    {
+        $this->data[$key] = $content;
+    }
+
+
     public function getTemplate(): string
     {
         return file_get_contents($this->file);
@@ -20,7 +58,7 @@ class View
 
     public function getContentHtml(): string
     {
-        $search = array_map(fn ($v) => '{{'.$v.'}}', array_keys($this->data));
+        $search = array_map(fn ($v) => '{{' . $v . '}}', array_keys($this->data));
         return str_replace($search, array_values($this->data), $this->getTemplate());
     }
 }
